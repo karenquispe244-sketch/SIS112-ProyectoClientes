c52dbd377fe8b35a8bc136f6ae7b705421b49bcb
Karen <karen.quispe244@gmail.com>
2026-06-18 00:50:33 -0400

    Refactorizacion: estructura modular, archivos binarios, seeding, busqueda recursiva, merge sort comparativo

--- src/utilidades.cpp
+++ src/utilidades.cpp
@@ -1,3 +1,8 @@
+// utilidades.cpp
+// Proposito: Funciones de utilidad general del sistema (menu, pantalla, pausa)
+// Parametros: ninguno
+// Retorno: void
+
 #include <iostream>
 #include <cstdlib>
 #include "utilidades.h"
@@ -5,21 +10,21 @@ using namespace std;
 
 void mostrarMenu() {
     cout << "\n========================================" << endl;
-    cout << "     SISTEMA DE CLIENTES EN C++         " << endl;
+    cout << "     SISTEMA DE GESTION DE CLIENTES     " << endl;
     cout << "========================================" << endl;
-    cout << "  [CRUD]" << endl;
+    cout << "  [CRUD - Archivos Binarios]" << endl;
     cout << "  1. Crear cliente" << endl;
     cout << "  2. Listar clientes" << endl;
     cout << "  3. Modificar cliente" << endl;
     cout << "  4. Eliminar cliente" << endl;
-    cout << "  [VECTORES + ORDENACION]" << endl;
-    cout << "  5. Ordenar clientes" << endl;
+    cout << "  [ORDENACION]" << endl;
+    cout << "  5. Ordenar / Comparar algoritmos" << endl;
     cout << "  [BUSQUEDA]" << endl;
-    cout << "  6. Busqueda secuencial" << endl;
-    cout << "  7. Busqueda binaria" << endl;
+    cout << "  6. Buscar cliente" << endl;
     cout << "  [INTERCALACION]" << endl;
-    cout << "  8. Intercalar archivos" << endl;
+    cout << "  7. Intercalar archivos" << endl;
     cout << "  [SISTEMA]" << endl;
+    cout << "  8. Reinicializar datos de prueba" << endl;
     cout << "  9. Salir" << endl;
     cout << "========================================" << endl;
     cout << "Opcion: ";
